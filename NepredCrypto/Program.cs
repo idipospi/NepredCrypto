58197a73b7f423f6dcfc186e2c3347b283c7adad
nepredumol <n.spyvak@gmail.com>
2024-02-29 22:08:06 +0300

    second commit

---
+++ NepredCrypto/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NepredCrypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is will be crypto program. It will be added soon.");
+        }
+    }
+}
+
