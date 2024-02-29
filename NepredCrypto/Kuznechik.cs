9fa7739df016e22dcd1a0070aa00fea5a517af75
nepredumol <n.spyvak@gmail.com>
2024-02-29 22:51:45 +0300

    Kuznechik realization

---
+++ NepredCrypto/Kuznechik.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NepredCrypto
+{
+    class Kuznechik
+    {
+        [DllImport("kuznyechik.dll")]
+        static extern void Kuznechik_Encrypt(byte[] key, byte[] buffer, int length);
+
+        [DllImport("kuznyechik.dll")]
+        static extern void Kuznechik_Decrypt(byte[] key, byte[] buffer, int length);
+
+        public static byte[] Encrypt(byte[] data, byte[] key)
+        {
+            if ((data.Length % 16) != 0)
+            {
+                int blocks_count = (data.Length / 16) + 1;
+                Array.Resize(ref data, blocks_count * 16);
+            }
+
+            Kuznechik_Encrypt(key, data, data.Length);
+            return data;
+        }
+        
+        public static byte[] Decrypt(byte[] data, byte[] key)
+        {
+            Kuznechik_Decrypt(key, data, data.Length);
+            return data;
+        }
+    }
+
+}
--- NepredCrypto/Program.cs
+++ NepredCrypto/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace NepredCrypto
+﻿using Org.BouncyCastle.Utilities.Encoders;
+namespace NepredCrypto
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("This is will be crypto program. It will be added soon.");
+            char[] keyChars = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', 
+            '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2'};
+            byte[] key = System.Text.Encoding.UTF8.GetBytes(keyChars);
+            string plaintext = "Hello, World!";
+            byte[] dataToEnc = System.Text.Encoding.UTF8.GetBytes(plaintext);
+            byte[] ciphertext = Kuznechik.Encrypt(dataToEnc, key);
+            Console.WriteLine(Hex.ToHexString(ciphertext));
         }
     }
 }
